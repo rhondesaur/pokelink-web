205c17dcb1665db6b9fa142c7bc0ab559b63efa7
rhondesaur <morganmullen22@gmail.com>
2022-02-17 17:01:08 -0600

    add morgan ironmon theme

---
+++ themes/morgan-ironmon/assets/js/themeSettings.js
@@ -0,0 +1,38 @@
+var themeSettings = {
+    theme: {
+        name: 'morgan-ironmon'
+    },
+
+    pokeImg: {
+        // will use species name instead (eg Bulbasaur.gif instead of 1.gif),
+        useDexNumbers: false,
+        // valid image types include: gif, jpg, jpeg, png etc
+        fileType: 'gif',
+        ignoreForms: true,
+        ignoreShinies: true,
+    },
+
+    // Change these to thwwwwwwwwe image paths
+
+    imgPaths: {
+      normal: 'https://assets.pokelink.xyz/assets/sprites/pokemon/gen7/animated/',
+      shiny: 'https://assets.pokelink.xyz/assets/sprites/pokemon/gen7/shiny/',
+      party: 'https://assets.pokelink.xyz/assets/sprites/pokemon/gen8/party/',
+      animatedEgg: 'https://assets.pokelink.xyz/assets/sprites/egg.gif',
+      staticEgg: 'https://www.cpokemon.com/pokes/home/0.png',
+      unknown: 'https://assets.pokelink.xyz/assets/sprites/',
+      badges: 'https://assets.pokelink.xyz/assets/sprites/badges/',
+      status: 'https://assets.pokelink.xyz/assets/sprites/status/',
+      types: 'https://assets.pokelink.xyz/assets/sprites/types/',
+      items: 'https://assets.pokelink.xyz/assets/sprites/items/',
+    },
+
+    typeColors: {
+        'bug': '#a8b820', 'dark': '#c02020', 'dragon': '#7038f8',
+        'electric': '#f8d030', 'fairy': '#ee99ac', 'fighting': '#c03028',
+        'fire': '#f08030', 'flying': '#a890f0', 'ghost': '#705898',
+        'grass': '#78c850', 'ground': '#e0c068', 'ice': '#98d8d8',
+        'normal': '#a8a878', 'poison': '#a040a0', 'psychic': '#f85888',
+        'rock': '#b8a038', 'steel': '#b8b8d0', '???': '#68a090', 'water': '#6890f0',
+      }, 
+};
